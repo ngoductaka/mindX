5a566e89363e15a7ab835d488b5b8d2c93d8e8a6
ngoc duc <ngocduc@ngocs-MacBook-Pro.local>
2021-04-16 22:41:48 +0700

    update code

--- b03-express-potman/express-b3/index.js
+++ b03-express-potman/express-b3/index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const router = require('./router');
+// const express = require('express');
+// const router = require('./router');
 
 
-const app = express()
-// https://runkit.com/
-const port = 3000
-app.use(express.static('anh'))
+// const app = express()
+// // https://runkit.com/
+// const port = 3000
+// app.use(express.static('anh'))
 
-app.use('/', router)
+// app.use('/', router)
 
-//  các method trên http
+// //  các method trên http
+
+// app.get('/user', (req, res) => {
+//     const {param} = request;
+//     res.json(param);
+// })
 
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
+

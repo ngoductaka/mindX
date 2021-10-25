d72d8ae94a2463596a31bc6a84ca9739d02ff1e7
ducdn <ducdn@gmail.com>
2021-10-25 22:02:44 +0700

    update code for tictactoe

--- b11-react-overview/b11-init-app/src/LiftUpState/liftUpState.js
+++ b11-react-overview/b11-init-app/src/LiftUpState/liftUpState.js
@@ -1,92 +1,39 @@
 import React, { useState, useEffect } from 'react';
-
 export const Temp = () => {
-	const [c, setC] = useState('');
-	const [f, setF] = useState('');
-
-	const _handleChangeVal = (type, val) => {
-		// input not number
-		if (isNaN(val)) return 0;
-
-		if (type == 'c') {
-			setC(val)
-			setF(toFahrenheit(val))
-		} else {
-			setC(toCelsius(val))
-			setF(val)
-		}
+	const [state, setState] = useState([
+		['', '', ''],
+		['', '', ''],
+		['', '', ''],
+	])
+	const _handleClick = (row, col) => {
+		console.log(row, col)
+		state[row][col] = 'X';
+		setState([...state]);
 	}
-
 	return (
-		<div>
-
-			<fieldset>
-				<legend>Độ C:</legend>
-				<input value={c} onChange={e => _handleChangeVal('c', e.target.value)} />
-			</fieldset>
-
-			<fieldset>
-				<legend>Độ F:</legend>
-				<input value={f} onChange={e => _handleChangeVal('f', e.target.value)} />
-			</fieldset>
-
+		<div style={{ margin: 20, }}>
+			{
+				state.map((item, row) => {
+					return <Row {...{ item, row, _handleClick }} />
+				})
+			}
 		</div>
 	)
 }
 
-
-function toCelsius(fahrenheit) {
-	return (fahrenheit - 32) * 5 / 9;
-}
-
-function toFahrenheit(celsius) {
-	return (celsius * 9 / 5) + 32;
-}
-
-
-export const TemSplit = () => {
-	const [c, setC] = useState('');
-	const [f, setF] = useState('');
-
-	const _handleChangeVal = (type, val) => {
-		if (isNaN(val)) return 0;
-
-		if (type == 'c') {
-			setC(val)
-			setF(toFahrenheit(val))
-		} else {
-			setC(toCelsius(val))
-			setF(val)
-		}
-
-	}
-
+const Row = ({ item, row, _handleClick }) => {
 	return (
-		<div>
-			<InputForm
-				label="Độ C:"
-				value={c}
-				onChange={val => _handleChangeVal('c', val)}
-			/>
-			<InputForm
-				label="Độ F:"
-				value={f}
-				onChange={val => _handleChangeVal('f', val)}
-			/>
+		<div style={{ display: 'flex' }}>
+			{item.map((i, col) => {
+				return <div
+					onClick={() => _handleClick(row, col)}
+					style={{
+						height: 70, width: 70, border: '1px solid #ddd',
+						display: 'flex', justifyContent: 'center', alignItems: 'center'
+					}}> {i}
+				</div>
+			})}
+
 		</div>
-	)
-
-}
-
-const InputForm = ({
-	label = "",
-	value = "",
-	onChange = () => { }
-}) => {
-	return (
-		<fieldset>
-			<legend>{label}</legend>
-			<input value={value} onChange={e => onChange(e.target.value)} />
-		</fieldset>
 	)
 }
